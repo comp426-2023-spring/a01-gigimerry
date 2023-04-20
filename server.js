3ef1815943b1eac2ce20ea18724d80f9e87482ed
gigimerry <69761091+gigimerry@users.noreply.github.com>
2023-04-20 15:13:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Require http module
 const http = require('http');
 // Require fs module
-
+const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
