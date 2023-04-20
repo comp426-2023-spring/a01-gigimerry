7aa64364c392b7fc2e7fb840fed66081646324e5
gigimerry <69761091+gigimerry@users.noreply.github.com>
2023-04-20 15:19:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,11 @@ fs.readFile('./public/index.html', 'utf8', (consoleError, fileData) => {
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-const server = http.createServer((req, res) => {}
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(file)
+  })
 
 
 
